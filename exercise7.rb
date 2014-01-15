a0f3b8dbd46646aee99155cf2fe122132c2ebabb
nunojorge <nujo@hotmail.com>
2014-01-15 15:10:20 -0500

    step 1

--- exercise7.rb
+++ exercise7.rb
@@ -3,3 +3,11 @@ students = {
 	:cohort2 => 42,
 	:cohort3 => 22
 }
+
+#Step 1 - display cohort name and student number
+def cohort_display(students)
+	students.each do |key, value|
+  		puts "#{key}: #{value} students"
+	end
+end
+cohort_display(students)
